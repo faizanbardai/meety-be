b5745d8a464014865e0ddfffdb62183ae8d16533
neha venkat <venkatsetty.neha@gmail.com>
2020-03-18 22:59:58 +0100

    imported list-end-points

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const passport = require("passport");
 const cors = require("cors");
+const listEndpoints = require("express-list-endpoints");
 
 const mongooseConnection = require("./src/db/mongoose");
 const bodyParser = require("body-parser");
@@ -9,8 +10,6 @@ const userRoute = require("./src/routes/user");
 const eventRoute = require("./src/routes/event");
 
 require("dotenv").config();
-
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,7 +20,7 @@ app.use(passport.initialize());
 
 app.use("/user", userRoute);
 app.use("/event", eventRoute);
-
+console.log(listEndpoints(app));
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
