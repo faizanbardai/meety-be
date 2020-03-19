05a4e6781963e9d756ed3a55ac8911fb797093d7
Faizan Bardai <faizan.bardai@celeritas-solutions.com>
2020-03-19 14:16:28 +0100

    Added schedule to event model

--- src/models/event.js
+++ src/models/event.js
@@ -3,18 +3,21 @@ const mongoose = require("mongoose");
 const eventSchema = new mongoose.Schema(
   {
     name: String,
-    duration: String,
+    schedule: Date,
+    duration: Number,
     picture: {
       type: String,
       default: "https://via.placeholder.com/300"
     },
     description: String,
     participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-    host: [{
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }],
-    price:  Number,
+    host: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    price: Number,
     like: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
     link: String,
     comments: [
